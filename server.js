ff1cb95c1838c64298d5383c2f5253ebe064905c
Alec Maly <32174181+alecjmaly@users.noreply.github.com>
2022-04-19 10:16:41 -0400

    added error handing

--- server.js
+++ server.js
@@ -7,45 +7,49 @@ const port = 80
 
 
 async function generateGithubHistoryRSS(res, url) {
-    let html = await fetch(url).then(resp => { return resp.text() })
-    let github_username = url.split('/')[3]
-    let github_repo = url.split('/')[4]
-    let root = parse(html)
-    // console.log(html)
+    try {
+        let html = await fetch(url).then(resp => { return resp.text() })
+        let github_username = url.split('/')[3]
+        let github_repo = url.split('/')[4]
+        let root = parse(html)
+        // console.log(html)
 
-    let rss = `<?xml version="1.0" encoding="utf-8"?>
-    <rss version="2.0">
-        <channel>
-            <title>${github_username} - ${github_repo}: github updates</title>
-            <link>${url}</link>
-            <description>Github history updates</description>
-    `
-
-
-    for (let timeline of root.querySelectorAll('.TimelineItem')) {
-        let date = timeline.querySelector('h2').text.replace('Commits on', '')
-        date = (new Date(date.trim()).toUTCString())
-        let title  = timeline.querySelector('a').text
-        title = encode(title, {mode: 'nonAsciiPrintable', level: 'xml'})
-        let item_url = 'https://github.com' +  timeline.querySelector('a').getAttribute('href')
-        rss += `
-                <item>
-                    <title>${title}</title>
-                    <pubDate>${date}</pubDate>
-                    <link>${item_url}</link>
-                    <description>${github_username} - ${github_repo}: Github update</description>
-                </item>
+        let rss = `<?xml version="1.0" encoding="utf-8"?>
+        <rss version="2.0">
+            <channel>
+                <title>${github_username} - ${github_repo}: github updates</title>
+                <link>${url}</link>
+                <description>Github history updates</description>
         `
-        // console.log(url)
+
+
+        for (let timeline of root.querySelectorAll('.TimelineItem')) {
+            let date = timeline.querySelector('h2').text.replace('Commits on', '')
+            date = (new Date(date.trim()).toUTCString())
+            let title  = timeline.querySelector('a').text
+            title = encode(title, {mode: 'nonAsciiPrintable', level: 'xml'})
+            let item_url = 'https://github.com' +  timeline.querySelector('a').getAttribute('href')
+            rss += `
+                    <item>
+                        <title>${title}</title>
+                        <pubDate>${date}</pubDate>
+                        <link>${item_url}</link>
+                        <description>${github_username} - ${github_repo}: Github update</description>
+                    </item>
+            `
+            // console.log(url)
+        }
+
+        rss += `</channel>
+        </rss>
+        `
+
+        // console.log(rss)
+        res.set('Content-Type', 'application/xml');
+        res.send(rss)
+    } catch {
+        res.send(`Failed to generate RSS for: ${url}`)
     }
-
-    rss += `</channel>
-    </rss>
-    `
-
-    // console.log(rss)
-    res.set('Content-Type', 'application/xml');
-    res.send(rss)
 }
 
 // generate github rss endpoint
